669e6332c933ecafc2b44b5dd8278d286559c114
Sonit <sonitbuilds+git@gmail.com>
2023-03-25 09:33:37 -0700

    head

--- nextapp/src/pages/why-us.tsx
+++ nextapp/src/pages/why-us.tsx
@@ -1,3 +1,16 @@
+import PageHeader from "@/components/PageHeader";
+import FooterSection from "@/components/sections/FooterSection";
+import GutterSection from "@/components/sections/GutterSection";
+import NavigationSection from "@/components/sections/NavigationSection";
+
 export default function WhyUs() {
-  return <div>Why us?</div>;
+  return (
+    <>
+      <PageHeader title="Why us" />
+      <NavigationSection />
+      <div>Why us?</div>
+      <FooterSection />
+      <GutterSection />
+    </>
+  );
 }
